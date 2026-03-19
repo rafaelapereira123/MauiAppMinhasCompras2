e8bbdbe5e5656b566101706d9565d2fe4208a131
rp431 <rp431@RAFAELA>
2026-03-18 23:03:17 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -0,0 +1,100 @@
+using MauiAppMinhasCompras.Models;
+using System.Collections.ObjectModel;
+
+namespace MauiAppMinhasCompras.Views;
+
+public partial class ListaProduto : ContentPage
+{
+    ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+
+    public ListaProduto()
+	{
+		InitializeComponent();
+        lst_produtos.ItemsSource = lista;
+    }
+
+
+    protected async override void OnAppearing()
+    {
+        try { 
+        List<Produto> tmp = await App.Db.GetAll();
+        tmp.ForEach(i => lista.Add(i));
+    }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "Ok");
+        }
+    }
+
+         private void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PushAsync(new Views.NovoProduto());
+        }
+        catch (Exception ex)
+        {
+             DisplayAlert("Ops", ex.Message, "Ok");
+        }
+
+    }
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        try
+        {
+            string q = e.NewTextValue;
+            lista.Clear();
+
+            List<Produto> tmp = await App.Db.GetAll();
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "Ok");
+        }
+
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+        double soma = lista.Sum(i => i.Total);
+        string msg = $"O total é {soma:C}";
+        DisplayAlert("Total dos Produtos", msg, "Ok");
+
+
+    }
+
+    private async void MenuItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            MenuItem selecionado = sender as MenuItem;
+            Produto p = selecionado.BindingContext as Produto;
+            bool confirm = await DisplayAlert("Tem certeza?", $"Remover {p.Descricao}", "Sim", "Não");
+            if (confirm)
+            {
+                await App.Db.Delete(p.Id);
+                lista.Remove(p);
+            }
+
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "Ok");
+        }
+    }
+
+    private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        try
+        {
+            Produto p = e.SelectedItem as Produto;
+            Navigation.PushAsync(new Views.EditarProduto
+            {
+                BindingContext = p,
+            });
+        }
+        catch (Exception ex)
+        { DisplayAlert("Ops", ex.Message, "Ok"); }
+    }
+}
---
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -0,0 +1,30 @@
+using MauiAppMinhasCompras.Models;
+using System.Threading.Tasks;
+namespace MauiAppMinhasCompras.Views;
+
+public partial class NovoProduto : ContentPage
+{
+    public NovoProduto()
+    {
+        InitializeComponent();
+    }
+    private async void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+        try
+        {
+            Produto p = new Produto
+            {
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+            await App.Db.Insert(p);
+            await DisplayAlert("Sucesso!", "Registro Inserido", "Ok");
+
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "Ok");
+        }
+    }
+}
